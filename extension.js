36e9796f7967e2d795371b968463de0cabd685ce
Çağlar Çavdar <caglar.cavdar@windowslive.com>
2024-09-08 12:27:44 +0200

    remove lines in the code

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,28 +9,75 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.codeCleanUp", () => {
+      // Get the active text editor
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-clean-up" is now active!');
+      // Get the configuration for the extension
+      const config = vscode.workspace.getConfiguration("codeCleanUp");
+      const wordsToRemove = config.get("wordsToRemove");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('code-clean-up.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      // Get the document and its text
+      const document = editor.document;
+      const text = document.getText();
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-clean-up!');
-	});
+      // Split the text into lines
+      const lines = text.split(/\r?\n/g);
 
-	context.subscriptions.push(disposable);
+      // Iterate through each line, checking if it contains any of the words to remove
+      const filteredLines = lines.filter((line) => {
+        return !wordsToRemove.some((word) => line.includes(word));
+      });
+
+      if (filteredLines.length === lines.length) {
+        return;
+      }
+      // Join the filtered lines back into a single string
+      const newText = filteredLines.join("\n");
+
+      // Replace the text in the document with the filtered text
+      editor.edit((editBuilder) => {
+        editBuilder.replace(
+          document.validateRange(new vscode.Range(0, 0, document.lineCount, 0)),
+          newText
+        );
+      });
+    })
+  );
+
+  // Add the context menu option
+  context.subscriptions.push(
+    vscode.languages.registerCodeActionsProvider(
+      [
+        { language: "javascript", scheme: "file" },
+        { language: "javascriptreact", scheme: "file" },
+        { language: "typescript", scheme: "file" },
+        { language: "typescriptreact", scheme: "file" },
+        { language: "vue", scheme: "file" },
+      ],
+      {
+        provideCodeActions: (document, range) => {
+          return [
+            {
+              command: "extension.codeCleanUp",
+              title: "Code Clean Up",
+              arguments: [document, range],
+            },
+          ];
+        },
+      }
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
